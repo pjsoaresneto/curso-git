86339d8c319f85b56aadef8234d637d905d99f50
Pedro Soares <pjsaoresneto@gmail.com>
2020-03-18 13:17:50 +0000

    Projeto iniciado mostrando Ola mundo

---
+++ projeto-teste-git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projeto_teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+        }
+    }
+}
