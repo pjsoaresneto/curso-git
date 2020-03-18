08b69c1706b76d24e1c8907a3629c6f31e075806
Pedro Soares <pjsaoresneto@gmail.com>
2020-03-18 13:31:42 +0000

    Acrescentada mensagem como voce esta

--- projeto-teste-git/Program.cs
+++ projeto-teste-git/Program.cs
@@ -8,6 +8,7 @@ namespace projeto_teste_git
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia.");
+            Console.WriteLine("Como você está?");
 
             Console.WriteLine();
         }
